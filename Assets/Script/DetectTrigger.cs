21b1e055ed98de729c097dc579f0485bc4556f87
Diamond26400 <obaloluwajames5@gmail.com>
2023-09-27 20:19:15 +0100

    update

--- Assets/Script/DetectTrigger.cs
+++ Assets/Script/DetectTrigger.cs
@@ -7,11 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void OntriggerEnter(Collider Other)                
+    void OntriggerEnter(Collider Other)
     {
         Destroy(gameObject);
         Destroy(Other.gameObject);
